f5aa7d48276ad77cadbf030d8e703d211069197d
pipu11qiao <18854558196@163.com>
2019-07-22 14:47:44 +0800

    add jsx skeleton

---
+++ templates/skeleton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+const Comp =  function (){
+    return (
+        <div>Comp</div>
+    )
+};
+Comp.defauttProps = {
+};
+Comp.propTypes = {
+};
+
+class Comp extends Component {
+    static defauttProps = {};
+    static propTypes = {};
+};
+
+export default Comp
